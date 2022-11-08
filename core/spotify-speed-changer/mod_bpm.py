dd5c71e4413b50a9751df264293197a3f80c8acd
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2022-11-08 12:29:52 +0000

    add usage to spotify-speed-changer

--- core/spotify-speed-changer/mod_bpm.py
+++ core/spotify-speed-changer/mod_bpm.py
@@ -2,10 +2,15 @@ import sys
 
 from spotify import Song
 
+args = sys.argv[1:]
+if len(args) < 1:
+    print('Usage: python mod_bpm.py <10>')
+    sys.exit(1)
+
 if __name__ == '__main__':
     input_file = 'top50.csv'
     output_file = 'top50_mod.csv'
-    relative_bpm = int(sys.argv[1])
+    relative_bpm = int(args[0])
 
     songs = Song.load_songs(input_file)
 
